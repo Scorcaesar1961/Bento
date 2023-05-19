757b11e2afdd66c3fadda73eef47af73a1c6793e
Scorcaesar1961 <133968335+Scorcaesar1961@users.noreply.github.com>
2023-05-19 15:39:12 +0200

    Update config.js

--- config.js
+++ config.js
@@ -30,6 +30,8 @@ const CONFIG = {
 	weatherIcons: 'Nord', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
+	weatherLatitude: '56.879005',
+	weatherLongitude: '14.805852',
 
 	trackLocation: false, // If false or an error occurs, the app will use the lat/lon below
 	defaultLatitude: '37.775',
