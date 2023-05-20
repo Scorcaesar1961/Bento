cdc23de598fc5695cdcfaa652e5cf76124489384
Alvin Johansson <alvin@donedev.se>
2023-05-20 17:35:07 +0200

    Update config.js

--- config.js
+++ config.js
@@ -33,9 +33,9 @@ const CONFIG = {
   weatherLatitude: "56.879005",
   weatherLongitude: "14.805852",
 
-  trackLocation: false, // If false or an error occurs, the app will use the lat/lon below
-  defaultLatitude: "37.775",
-  defaultLongitude: "-122.419",
+  trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
+  defaultLatitude: "56.879005",
+  defaultLongitude: "14.805852",
 
   // Autochange
   autoChangeTheme: true,
