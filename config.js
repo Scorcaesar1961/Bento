202be76565d247022699d16a07142570bd2c2cdb
Scorcaesar1961 <133968335+Scorcaesar1961@users.noreply.github.com>
2023-05-20 18:15:29 +0200

    Update config.js

--- config.js
+++ config.js
@@ -17,10 +17,10 @@ const CONFIG = {
   twelveHourFormat: false,
 
   // Greetings
-  greetingMorning: "Good morning!",
-  greetingAfternoon: "Good afternoon,",
-  greetingEvening: "Good evening,",
-  greetingNight: "Go to Sleep!",
+  greetingMorning: "Gomorron!",
+  greetingAfternoon: "Go'eftermiddag,",
+  greetingEvening: "Gokväll,",
+  greetingNight: "Gå å lägg dig!",
 
   // Layout
   bentoLayout: "bento", // 'bento', 'lists', 'buttons'
